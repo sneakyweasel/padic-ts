451566b33b2b23b79f4950b17b80551de5fda19e
Philippe Cochin <philippe.cochin@gmail.com>
2021-08-11 15:06:49 +0200

    Upgraded expansion

--- src/components/PadicExpansion.vue
+++ src/components/PadicExpansion.vue
@@ -2,18 +2,33 @@
   <div>
     <h2 class="title">{{ prime }}-adic expansion: a new way to write numbers!</h2>
 
-    <!-- Prime factorization -->
+    <!-- Classical sum -->
+    <p class="intro">We usually represent fractions using decimal representation in base ten</p>
+    <!-- Fraction input -->
+    <div class="flex items-center mr-3">
+      <div class="w-1/3 definition">Decimal expansion</div>
+      <div class="w-2/3 dynamic" v-katex:display="decimalKatex"></div>
+    </div>
+    <div class="flex items-center mt-3">
+      <div class="w-1/3 definition">Infinite sum of 10<sup>x</sup></div>
+      <div class="w-2/3 dynamic" v-katex:display="classicalSumKatex"></div>
+    </div>
+
+    <!-- Padic sum -->
     <p class="intro">
       With our new distance idea we can now rewrite numbers by their closeness to p={{ prime }}
     </p>
     <div class="flex items-center mt-3">
-      <div class="w-1/3 definition">{{ prime }}-adic infinite sum</div>
-      <div class="w-2/3 dynamic" v-katex:display="explainSumKatex + '=' + expandedSumKatex"></div>
-    </div>
-
-    <div class="flex items-center mt-3">
       <div class="w-1/3 definition">{{ prime }}-adic expansion (ltr)</div>
       <div class="w-2/3 dynamic" v-katex:display="padic.toKatex()"></div>
+    </div>
+    <div class="flex items-center mt-3">
+      <div class="w-1/3 definition">{{ prime }}-adic infinite sum</div>
+      <div class="w-2/3 dynamic" v-katex:display="expandedSumKatex"></div>
+    </div>
+    <div class="flex items-center mt-3">
+      <div class="w-1/3 definition">{{ prime }}-adic valuation</div>
+      <div class="w-2/3 dynamic" v-katex:display="padicValuation"></div>
     </div>
 
     <!-- Computation steps -->
@@ -63,8 +78,16 @@ export default class PadicExpansion extends Vue {
   @Prop() readonly prime!: number
   @Prop() readonly precision!: number
 
+  get num(): string {
+    return this.ratio.toFixed(4)
+  }
+
   get padic(): Padic {
     return this.ratio.toPadic(this.prime)
+  }
+
+  get padicValuation(): string {
+    return this.ratio.padicValuation(this.prime).toString()
   }
 
   get padicExpansion(): Step[] {
@@ -75,8 +98,35 @@ export default class PadicExpansion extends Vue {
     return this.padic.toArray().join(' ')
   }
 
+  get decimalKatex(): string {
+    return `${this.ratio.toKatex()} \\approx \\textcolor{purple}{${this.num}}`
+  }
+
+  get classicalSumKatex(): string {
+    let result = `\\sum_{\\textcolor{red}{k}=n}^{\\infin} \\textcolor{purple}{digit}_{\\textcolor{red}{k}} \\cdot \\textcolor{blue}{10}^{\\textcolor{red}{k}} =`
+    let intPart = ''
+    let decPart = ''
+    if (this.num.toString().includes('.')) {
+      intPart = this.num.toString().split('.')[0]
+      decPart = this.num.toString().split('.')[1]
+    } else {
+      intPart = this.num.toString().split('.')[0]
+    }
+    intPart.split('').forEach((digit, index) => {
+      result += `\\textcolor{purple}{${digit}} \\cdot \\textcolor{blue}{10}^{\\textcolor{red}{${
+        intPart.length - index - 1
+      }}} + `
+    })
+    decPart.split('').forEach((digit, index) => {
+      result += `\\textcolor{purple}{${digit}} \\cdot \\textcolor{blue}{10}^{\\textcolor{red}{-${
+        index + 1
+      }}} + `
+    })
+    return result.slice(0, -3)
+  }
+
   get expandedSumKatex(): string {
-    let result = ''
+    let result = `\\sum_{\\textcolor{red}{k}=n}^{\\infin} \\textcolor{purple}{digit}_{\\textcolor{red}{k}} \\cdot \\textcolor{blue}{p}^{\\textcolor{red}{k}} = `
     this.padicExpansion.slice(0, 5).forEach((step, index) => {
       result += this.sumKatex(step, index)
       result += ' + '
@@ -85,12 +135,8 @@ export default class PadicExpansion extends Vue {
     return result
   }
 
-  get explainSumKatex(): string {
-    return `\\sum_{\\textcolor{red}{k}=n}^{\\infin} a_{\\textcolor{red}{k}} \\cdot \\textcolor{blue}{p}^{\\textcolor{red}{k}}`
-  }
-
   stepKatex(step: Step, index: number): string {
-    return `Step \\space \\textcolor{red}{${index}}`
+    return `Digit \\space \\textcolor{red}{${index}}`
   }
 
   sumKatex(step: Step, index: number): string {
